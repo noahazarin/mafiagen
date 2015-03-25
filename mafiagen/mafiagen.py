ad10409189ca6db97b6bf57d1407eddba1b0658d
Noah Azarin <noahazarin@gmail.com>
2015-03-24 17:56:08 -0700

    cleanup

--- mafiagen/mafiagen.py
+++ mafiagen/mafiagen.py
@@ -4,21 +4,27 @@ def read_template_file():
     '''
     pass
 
-def read_setup_file():
+def read_setup_file(filename):
     '''
     read file containing setup specific, like number of mafia, number of town, and so on.
     '''
-    pass
+    result = []
+    with open("ini/%s" % filename, "r") as f:
+        for line in f:
+            result.append(line)
+    return ''.join(result)
 
 def write_file(poststring, params, filename):
     '''
     write files with final mafia setup posts to /tmp
     uses poststring % params, so pass params as a tuple if sending more than one variable (which is pretty much always) 
     '''
-    with open("tmp/%s" % filename, 'w+') as f:
+    with open("tmp/%s" % filename, "w+") as f:
         outlines = poststring % params
         f.write(poststring % params)
         f.seek(0)
         result = f.read()
         f.close()
     return result 
+
+
--- tests/mafiagen_tests.py
+++ tests/mafiagen_tests.py
@@ -14,8 +14,9 @@ def test_template_read():
     mafiagen.read_template_file()
 
 def test_setup_read():
-    mafiagen.read_setup_file()
-
+    result = mafiagen.read_setup_file("testfile.txt")
+    assert_equal(result, "test file line 1\ntest file line 2\ntest file line 3\n")
+    
 def test_write_file():
     result = mafiagen.write_file("pudding %s", "is delicious", "testfile.txt")
     assert_equal(result, "pudding is delicious")
